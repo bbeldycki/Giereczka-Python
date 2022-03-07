0e0f160af77a93851a46925b41dd01e60ebfffbd
Bartosz Beldycki <bartosz.beldycki@acaisoft.com>
2022-03-07 18:25:24 +0100

    Creating MyServer and RespondeHandler classes

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
+import select
 import socket
 import threading
 import pickle
+import time
+
 from client.server_validate_position import *
+from typing import Dict, Any
 import sys
 
 header = 64
@@ -12,6 +16,7 @@ port = 5050
 addr = (server, port)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(addr)
+SERVER_TIMEOUT = 0.001
 
 
 def handle_client(connection, address):
@@ -64,3 +69,89 @@ def start():
 
 print('[STARTING] server is starting...')
 start()
+
+
+class ResponseHandler:
+    pass
+
+
+class MyServer:
+    def __init__(self, host: str, port: int, response_handler: ResponseHandler) -> None:
+        self.header = 1024 # default header length
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # server Socket
+        self.host = host
+        self.port = port
+        self.response_handler = response_handler
+        self.status = {'working': True, 'players': []}
+        self.__total_player_count = 0
+
+        try:
+            self.server.bind((host, port))
+            self.server.listen()
+            self.server.settimeout(5.0)
+        except Exception as error:
+            print(f'[ERROR IN CREATING SERVER] {error}')
+            sys.exit()
+        else:
+            print(f'[SERVER IS LISTENING] @ {host}:{port}')
+
+    def run(self) -> None:
+        while True:
+            try:
+                client, address = self.server.accept()
+                self.__total_player_count += 1
+
+                self._establish_connection(address, time.time())
+
+                connection_thread = threading.Thread(target=self._handler, args=(client, self.status))
+                connection_thread.start()
+            except socket.timeout:
+                continue
+            except KeyboardInterrupt:
+                socket.close()
+                sys.exit()
+            except BaseException as error:
+                print(f'[SERVER EXCEPTION] {error}')
+                socket.close()
+                sys.exit()
+
+    def _handler(self, connection: socket.socket, status: Dict[str, Any]) -> None:
+        while True:
+            ready_sockets, _, _ = select.select([connection], [], [], SERVER_TIMEOUT)
+
+            if not ready_sockets:
+                connection.send(pickle.dumps(status))
+                continue
+
+            try:
+                response = pickle.loads(connection.recv(self.header))
+                handler = getattr(self.response_handler, f'handle_{response["action"]}', None)
+                if callable(handler):
+                    status = handler(status, response['value'])
+                    connection.send(pickle.dumps(status))
+                else:
+                    print(f'NO HANDLER FOR {response["action"]}')
+            except Exception as e:
+                print(f'[EXCEPTION] {e}')
+                break
+
+    @staticmethod
+    def _establish_connection(address: str, connection_time: float) -> None:
+        print(f'CONNECTION HAS BEEN ESTABLISHED WITH: {address} at {connection_time}.')
+
+    @property
+    def total_player_count(self) -> int:
+        return self.__total_player_count
+
+    @property
+    def active_player_count(self) -> int:
+        return threading.active_count() - 1 # Substract 1 to remove the main thread
+
+
+def main() -> None:
+    server = MyServer()
+    server.run()
+
+
+if __name__ == '__main__':
+    main()
