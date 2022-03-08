b595653e7315b068714e522f7413f4c6a54617e8
Bartosz Beldycki <bartosz.beldycki@acaisoft.com>
2022-03-08 10:49:35 +0100

    Added methods to ReponseHandler class. Commented older code.

--- server.py
+++ server.py
@@ -8,77 +8,94 @@ from client.server_validate_position import *
 from typing import Dict, Any
 import sys
 
-header = 64
-formatter = 'utf-8'
-disconnect_message = 'disconnect'
-server = socket.gethostbyname(socket.gethostname())
-port = 5050
-addr = (server, port)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(addr)
+# header = 64
+# formatter = 'utf-8'
+# disconnect_message = 'disconnect'
+# server = socket.gethostbyname(socket.gethostname())
+# port = 5050
+# addr = (server, port)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind(addr)
 SERVER_TIMEOUT = 0.001
 
 
-def handle_client(connection, address):
-    print(f'[NEW CONNECTION] {address} connected.')
-
-    connected = True
-    while connected:
-        msg_length = connection.recv(header).decode(formatter)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = connection.recv(msg_length).decode(formatter)
-            if msg == disconnect_message:
-                connected = False
-            print(f'[{address}] {msg}')
-            connection.send('Msg received'.encode(formatter))
-    connection.close()
-
-
-def handle_client_pickle(connection, address):
-    print(f'[NEW CONNECTION] {address} connected.')
-
-    connected = True
-    while connected:
-        msg_received = connection.recv(1024)
-        if msg_received:
-            msg = pickle.loads(msg_received)
-            print(f'[{address}] {msg}')
-            if msg.get('disconnects', False):
-                connected = False
-            if msg.get('actual_player_position', [0, 0]) and msg.get('next_player_position', [0, 0]):
-                if can_move_there(msg.get('next_player_position', [0, 0])):
-                    return_value = msg.get('next_player_position', [0, 0])
-                    connection.send(pickle.dumps(return_value))
-                else:
-                    return_value = msg.get('next_player_position', [0, 0])
-                    connection.send(pickle.dumps(return_value))
-            # connection.send(pickle.dumps('Message received!'))
-    connection.close()
-
-
-def start():
-    s.listen()
-    print(f'[LISTENING] Server is listening on {server}')
-    while True:
-        connection, address = s.accept()
-        thread = threading.Thread(target=handle_client_pickle, args=(connection, address))
-        thread.start()
-        print(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
-
-
-print('[STARTING] server is starting...')
-start()
+# def handle_client(connection, address):
+#     print(f'[NEW CONNECTION] {address} connected.')
+#
+#     connected = True
+#     while connected:
+#         msg_length = connection.recv(header).decode(formatter)
+#         if msg_length:
+#             msg_length = int(msg_length)
+#             msg = connection.recv(msg_length).decode(formatter)
+#             if msg == disconnect_message:
+#                 connected = False
+#             print(f'[{address}] {msg}')
+#             connection.send('Msg received'.encode(formatter))
+#     connection.close()
+#
+#
+# def handle_client_pickle(connection, address):
+#     print(f'[NEW CONNECTION] {address} connected.')
+#
+#     connected = True
+#     while connected:
+#         msg_received = connection.recv(1024)
+#         if msg_received:
+#             msg = pickle.loads(msg_received)
+#             print(f'[{address}] {msg}')
+#             if msg.get('disconnects', False):
+#                 connected = False
+#             if msg.get('actual_player_position', [0, 0]) and msg.get('next_player_position', [0, 0]):
+#                 if can_move_there(msg.get('next_player_position', [0, 0])):
+#                     return_value = msg.get('next_player_position', [0, 0])
+#                     connection.send(pickle.dumps(return_value))
+#                 else:
+#                     return_value = msg.get('next_player_position', [0, 0])
+#                     connection.send(pickle.dumps(return_value))
+#             # connection.send(pickle.dumps('Message received!'))
+#     connection.close()
+#
+#
+# def start():
+#     s.listen()
+#     print(f'[LISTENING] Server is listening on {server}')
+#     while True:
+#         connection, address = s.accept()
+#         thread = threading.Thread(target=handle_client_pickle, args=(connection, address))
+#         thread.start()
+#         print(f'[ACTIVE CONNECTIONS] {threading.activeCount() - 1}')
+#
+#
+# print('[STARTING] server is starting...')
+# start()
 
 
 class ResponseHandler:
-    pass
+    def handle_commands(self, status: Dict[str, Any], response: Dict[str, Any]) -> Dict[str, Any]:
+        for command in response['commands']:
+            cmd = list(command.keys())[0]
+            handler = getattr(self, f'_handle_{cmd}', None)
+
+            if callable(handler):
+                status = handler(status, command[cmd])
+            else:
+                print(f'[NO HANDLER FOR] {cmd}')
+        return status
+
+    @staticmethod
+    def _handle_movement(status: Dict[str, Any], command: Dict[str, Any]) -> Dict[str, Any]:
+        for player in status['players']:
+            if player['id'] == command['id']:
+                player['pos'] = command['pos']
+                player['dir'] = command['dir']
+        return status
 
 
 class MyServer:
     def __init__(self, host: str, port: int, response_handler: ResponseHandler) -> None:
-        self.header = 1024 # default header length
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # server Socket
+        self.header = 1024  # default header length
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # server Socket
         self.host = host
         self.port = port
         self.response_handler = response_handler
@@ -108,11 +125,11 @@ class MyServer:
             except socket.timeout:
                 continue
             except KeyboardInterrupt:
-                socket.close()
+                self.socket.close()
                 sys.exit()
             except BaseException as error:
                 print(f'[SERVER EXCEPTION] {error}')
-                socket.close()
+                self.socket.close()
                 sys.exit()
 
     def _handler(self, connection: socket.socket, status: Dict[str, Any]) -> None:
@@ -145,7 +162,7 @@ class MyServer:
 
     @property
     def active_player_count(self) -> int:
-        return threading.active_count() - 1 # Substract 1 to remove the main thread
+        return threading.active_count() - 1  # Subtract 1 to remove the main thread
 
 
 def main() -> None:
